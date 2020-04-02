0800b515180e2918c963b3888b08ff10133192f6
aiswariya sugavanam <43380389+Aiswariyasugavanam@users.noreply.github.com>
2020-04-02 17:00:20 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,55 @@
+var request = require('request')
+var express = require('express')
+var bodyparser = require('body-parser')
+var app = express()
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
+var path= require('path')
+var server =require('http').createServer(app); 
+const io = require('socket.io')(server);
+app.post('/weather',function(req,res){
+    console.log("Received");
+    if(!req.body) return res.sendStatus(400)
+    res.setHeader('Content-Type','application/json')
+    var city = req.body.queryResult.parameters['geo-city'];
+    //var city = req.body.geocity
+    console.log(city)
+    var w = getWeather(city)
+    let response = " "
+    let responseObj = {
+        "fulfillmentText":response,
+        "fulfillmentMessages":[{"text":{"text":{w}}}],
+        "source":""
+    }
+    return res.json(responseObj)
+})
+var apiKey = '18063167efa4964644a6e994fafff9c4'
+var result 
+function cb(err,response,body){
+    if(err)
+    console.log('error',err)
+    var weather = JSON.parse(body)
+    console.log(weather)
+    if(weather.message === 'city not found'){
+        result = 'unable '+weather.message;
+    }
+    else{
+        result = 'Right now its'+weather.main.temp+'degrees wiyth'
+    }
+}
+function getWeather(city){
+    result = undefined
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    console.log(url)
+    var req = request(url,cb)
+    while(result == undefined){
+        require('deasync').runLoopOnce();
+    }
+    return result
+}
+app.get('/',function(req,res){
+    res.sendFile(__dirname+'/index.html')
+})
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+  })
